2a09e34656c43ca7f20bd3ef60fe7870fde30826
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-05-22 21:06:15 -0600

    CREATE: Models/reseña, usuario

---
+++ CatalogoApp.Domain/Models/Resena.cs
@@ -0,0 +1,12 @@
+﻿namespace CatalogoApp.Domain.Models
+{
+    public class Resena
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public string NombreUsuario { get; set; } = string.Empty;
+        public int Calificacion { get; set; } // 1-5
+        public string Comentario { get; set; } = string.Empty;
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    }
+}
---
+++ CatalogoApp.Domain/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogoApp.Domain.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
